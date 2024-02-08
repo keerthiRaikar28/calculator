0f969be0c5c67547e538215daec1fbf345d5e3d0
keerthi raikar <akashhulakund335@gmail.com>
2024-02-08 11:51:18 +0530

    updated code with square and cube functionalities

--- calculator.java
+++ calculator.java
@@ -19,10 +19,17 @@ class calculator{
         float division(float operand_1,float operand_2){
             return(operand_1 / operand_2);
             }
+        float square(float operand){
+            return (operand*operand);
+           }
+           float cube(float operand){
+            return (operand*operand*operand);
+           }
 
             public static void main(String[] args) {
                 float operand_1 = 10;
                 float operand_2 = 5;
+                float  operand = 2;
                 calculator calc  = new calculator();
                 float add_result = calc.addition(operand_1, operand_2);
                 System.out.println("result of addition is:" + add_result );
@@ -32,5 +39,9 @@ class calculator{
                 System.out.println("result of multiplication is:" + mul_result );
                 float div_result = calc.division(operand_1, operand_2);
                 System.out.println("result of division is:" + div_result );
+                float sq_result = calc.square(operand);
+                System.out.println("result of square is:" + sq_result );
+                float cube_result = calc.cube(operand);
+                System.out.println("result of cube is:" + cube_result );
             }
 }
